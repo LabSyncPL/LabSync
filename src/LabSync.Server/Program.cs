5f4c26d11a87c6b6c251e0b7e98aa62b36a84539
MichMajster <michmaster11@gmail.com>
2026-01-21 17:16:25 +0100

    zaktualizowano migrację danych

--- src/LabSync.Server/Program.cs
+++ src/LabSync.Server/Program.cs
@@ -23,6 +23,13 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<LabSyncDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
