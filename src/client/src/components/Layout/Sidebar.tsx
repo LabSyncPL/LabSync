4a9579893cd236e71b1256951aeed301039602d1
MichMajster <michmaster11@gmail.com>
2026-05-21 17:29:20 +0200

    adjusted logo size

--- src/client/src/components/Layout/Sidebar.tsx
+++ src/client/src/components/Layout/Sidebar.tsx
@@ -70,7 +70,7 @@ export function Sidebar() {
         <img
           src="/LabSyncLogoH.svg"
           alt="LabSync"
-          className="h-16 w-auto max-w-full object-contain"
+          className="h-12 w-auto max-w-full object-contain"
         />
       </div>
 
